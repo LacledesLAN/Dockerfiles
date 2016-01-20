3ea09093a827732275ff860d0ae474c000c79fae
James Dudley <jamesd-udley@users.noreply.github.com>
2016-01-20 16:50:04 -0600

    Update rebuild-all.sh

--- linux/rebuild-all.sh
+++ linux/rebuild-all.sh
@@ -4,4 +4,6 @@ docker rm -f $(docker ps -a -q)   #todo: add filter for ll/*
 echo "Destroying all LL docker images"
 docker rmi $(docker images -q)   #todo: add filter for ll/*
 
+docker build -t ll/gamesvr ./gamesvr 
+
 echo "done"
