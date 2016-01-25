797269a1fe2a6a29e2fb5d49a9c51c8257a5b84d
James Dudley <jedudley@outlook.com>
2016-01-25 13:58:31 -0600

    Temporary fix to update bug

--- linux/rebuild-all.sh
+++ linux/rebuild-all.sh
@@ -157,9 +157,8 @@ clear;
 if [ $script_skip_update != true ] ; then
 	find $script_directory -name \*dockerfile* -type f -delete	#can be removed once BEan's machines are clean of all instances of improperly-cased "dockerfile"
 	update_script;
-	bash "$script_fullpath" -z;		#Recursively re-run script; disable auto updating to prevent endless loop
-	echo "HIYA";
-	exit 0;
+	#bash "$script_fullpath" -z;		#Recursively re-run script; disable auto updating to prevent endless loop
+	#exit 0;
 fi
 
 draw_horizontal_rule;
