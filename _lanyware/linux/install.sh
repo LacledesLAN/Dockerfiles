4b3064dd59a22c713de3e112110e72cf1dac0e3a
James Dudley <jedudley@outlook.com>
2016-03-06 01:17:12 -0600

    Removed install.sh; added to readme.md

--- _lanyware/linux/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#basic setup script to go from zero to ready for the LL server managment system
-
-apt-get install -y curl discus htop git libc6-i386 lib32gcc1 lib32stdc++6 lib32tinfo5 lib32z1 tar tree wget
-curl -sSL https://get.docker.com/ | sh
-sudo usermod -aG docker sysoper
