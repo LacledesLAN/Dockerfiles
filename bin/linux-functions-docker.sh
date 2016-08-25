9d3fda19b45ce8c12b7d64c3100a1a1f56cba6c5
James Dudley <jedudley@outlook.com>
2016-08-24 22:39:11 -0500

    Formatting fixes

--- bin/linux-functions-docker.sh
+++ bin/linux-functions-docker.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Determines if Docker is installed
+function docker_installed() {
+
+    # Welcome to Linux; where 0 = true :/
+    command -v docker > /dev/null 2>&1 || { return 1; }
+
+    return 0;
+}
+
+
+# Remove a Docker image from the system; including all derived images
+# $1 The fully qualified name of the docker image to remove
 function remove_docker_image() {
-    command -v docker > /dev/null 2>&1 || { echo >&2 "Docker is required.  Aborting."; return 255; }
+    command -v docker > /dev/null 2>&1 || { echo >&2 "Docker is required.  Aborting."; return 123; }
 
     image_count=$(docker images $1 | grep -o "$1" | wc -l);
 
